9fc3c139d26ffd726d1106b732d2512339b87777
Aswin Jim Thuruthippilly <118459613+RealNickey@users.noreply.github.com>
2025-08-19 14:46:26 +0530

    Remove leftover app router files

---
+++ pages/_app.tsx
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { ClerkProvider } from '@clerk/nextjs';
+import "../src/tailwind.css"; // Tailwind layers (base, components, utilities)
+import "../src/index.css"; // Legacy global resets
+import "../src/App.scss"; // CSS variables & legacy styles
+import "../src/components/audio-pulse/audio-pulse.scss";
+import "../src/components/logger/logger.scss";
+import "../src/components/side-panel/side-panel.scss";
+import "../src/components/side-panel/react-select.scss";
+import "../src/components/settings-dialog/settings-dialog.scss";
+import "../src/components/control-tray/control-tray.scss";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Initialize any global settings or web vitals reporting here
+    const reportWebVitals = async (metric: any) => {
+      // You can send metrics to an analytics endpoint
+      console.log(metric);
+    };
+
+    // Call reportWebVitals if needed
+    // reportWebVitals();
+  }, []);
+
+  return (
+    <ClerkProvider>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
+
+export default MyApp;
